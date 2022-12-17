d6dc59ee743ca107dc326eb1da5f442a7a81b0f5
sujeethn <sujeethn@gmail.com>
2022-12-16 21:33:15 -0800

    Updated working version

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi test, {name}')  # Press ⌘F8 to toggle the breakpoint.
     # Convert the text to a matrix of word counts
     name = [name]
-    vectorizer = CountVectorizer().fit_transform(name)
+   # vectorizer = CountVectorizer().fit_transform(name)
 
     # Calculate the cosine similarity between the two texts
-    similarity = cosine_similarity(vectorizer[0:1], vectorizer[1:2])
+  #  similarity = cosine_similarity(vectorizer[0:1], vectorizer[1:2])
 
     # Print the similarity score
-    print(similarity)
+   # print(similarity)
 
 
 # Press the green button in the gutter to run the script.
